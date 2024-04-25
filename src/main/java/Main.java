2dccfaea958ba6207feccf371224b32de378d001
Hai Nguyen <nngochaial@gmail.com>
2024-04-26 02:04:49 +0700

    Connected to Supabase

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,20 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("my-persistence-unit");
+        EntityManager entityManager = entityManagerFactory.createEntityManager(); // Represents the context
+
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
+
+    }
 }
