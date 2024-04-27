95f78fa0255b323ce475588227c8e9b0a4aa0c85
Hai Nguyen <nngochaial@gmail.com>
2024-04-27 13:28:56 +0700

    Generate Dependent class

--- src/main/java/org/nikisurance/entity/Dependent.java
+++ src/main/java/org/nikisurance/entity/Dependent.java
@@ -1,7 +1,17 @@
 package org.nikisurance.entity;
 
-public class Dependent extends Customer {
-    public Dependent(String customerID, String fullName, String password, InsuranceCard insuranceCard) {
-        super(customerID, fullName, password, insuranceCard);
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+import java.io.Serializable;
+
+@Entity
+@DiscriminatorValue("dependent")
+public class Dependent extends Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
+    public Dependent() {}
+
+    public Dependent(String customerID, String fullName, String password) {
+        super(customerID, fullName, password);
     }
 }
