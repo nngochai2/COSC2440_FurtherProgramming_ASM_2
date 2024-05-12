c1a6aa1b7490421f1f2d198928bb3685da23f753
Hai Nguyen <nngochaial@gmail.com>
2024-05-12 21:58:20 +0700

    Update in PolicyOwner

--- src/main/java/org/nikisurance/entity/PolicyOwner.java
+++ src/main/java/org/nikisurance/entity/PolicyOwner.java
@@ -4,28 +4,22 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "policy_owner")
 public class PolicyOwner extends Customer implements Serializable {
 
     @OneToMany(mappedBy = "policyOwner", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
-    private List<PolicyHolder> policyHolders;
+    private Set<Beneficiary> beneficiaries;
 
     public PolicyOwner() {}
-    public List<PolicyHolder> getPolicyHolders() {
-        return policyHolders;
+
+    public Set<Beneficiary> getBeneficiaries() {
+        return beneficiaries;
     }
 
-    public void setPolicyHolders(List<PolicyHolder> policyHolders) {
-        this.policyHolders = policyHolders;
-    }
-
-    public void addPolicyHolder(PolicyHolder policyHolder) {
-        policyHolders.add(policyHolder);
-    }
-
-    public void removePolicyHolder(PolicyHolder policyHolder) {
-        policyHolders.remove(policyHolder);
+    public void setBeneficiaries(Set<Beneficiary> beneficiaries) {
+        this.beneficiaries = beneficiaries;
     }
 }
