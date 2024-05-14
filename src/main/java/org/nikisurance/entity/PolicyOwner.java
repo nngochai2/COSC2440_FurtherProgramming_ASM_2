fb22025a1659afd35492632991f11877a92cce13
Hai Nguyen <nngochaial@gmail.com>
2024-05-15 00:04:42 +0700

    Update in PolicyOwner

--- src/main/java/org/nikisurance/entity/PolicyOwner.java
+++ src/main/java/org/nikisurance/entity/PolicyOwner.java
@@ -9,7 +9,11 @@ import java.util.Set;
 @Table(name = "policy_owner")
 public class PolicyOwner extends Customer implements Serializable {
 
+    @Column(name = "base_premium", nullable = false)
+    private Long basePremium;
 
+    @Column(name = "dependent_rate", nullable = false)
+    private double dependentRate;
 
     @OneToMany(mappedBy = "policyOwner", cascade = CascadeType.MERGE, fetch = FetchType.LAZY, orphanRemoval = true)
     private Set<Beneficiary> beneficiaries;
@@ -22,5 +26,21 @@ public class PolicyOwner extends Customer implements Serializable {
 
     public void setBeneficiaries(Set<Beneficiary> beneficiaries) {
         this.beneficiaries = beneficiaries;
+    }
+
+    public Long getBasePremium() {
+        return basePremium;
+    }
+
+    public void setBasePremium(Long basePremium) {
+        this.basePremium = basePremium;
+    }
+
+    public double getDependentRate() {
+        return dependentRate;
+    }
+
+    public void setDependentRate(double dependentRate) {
+        this.dependentRate = dependentRate;
     }
 }
